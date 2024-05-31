eec36a8248d9fc99dc3259c9d535e1d882c79d89
Hemangshu-Dey <hemangshu1904@gmail.com>
2024-05-31 20:43:19 +0530

    eighth commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import { BrowserRouter as Router, Routes ,Route} from 'react-router-dom'
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+import Home from './components/Home';
+
+import { Toaster } from "react-hot-toast"
+import ProductDetails from './components/product/ProductDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Toaster position="top-center"/>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/product/:id" element={<ProductDetails />}/>
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </ Router>
   );
 }
 
